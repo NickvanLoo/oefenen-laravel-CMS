8e31df305fd8cecc7820d2bb6f7ae52ef6497666
NickvanLoo28 <58033512+NickvanLoo@users.noreply.github.com>
2022-03-03 12:30:37 +0100

    Update

---
+++ oefenen-laravel-CMS/oefenen-laravel-CMSblog/resources/views/pages/about.blade.php
@@ -0,0 +1,3 @@
+<h3>Over ons</h3>
+
+<p>Dit is de over ons pagina</p>
---
+++ oefenen-laravel-CMS/oefenen-laravel-CMSblog/resources/views/pages/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+<h1>Welkom op de Larablog</h1>
+<p>Dit is een blog applicatie gemaakt met Laravel om te oefenen met een laravel CMS.</p>
+
+@endsection
