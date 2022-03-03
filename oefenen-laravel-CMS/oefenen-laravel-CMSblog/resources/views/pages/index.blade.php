1cf6904a5189fe16e46b995b3fc0b8b60e60d7fc
NickvanLoo28 <58033512+NickvanLoo@users.noreply.github.com>
2022-03-03 16:35:43 +0100

    Update

--- oefenen-laravel-CMS/oefenen-laravel-CMSblog/resources/views/pages/index.blade.php
+++ oefenen-laravel-CMS/oefenen-laravel-CMSblog/resources/views/pages/index.blade.php
@@ -1,7 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="jumbotron text-center">
 <h1>Welkom op de Larablog</h1>
 <p>Dit is een blog applicatie gemaakt met Laravel om te oefenen met een laravel CMS.</p>
+</div>
 
 @endsection
